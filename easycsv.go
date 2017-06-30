246482b6a727cf646b1fe63528f03ef36e5122d0
Scott Ware <scottdware@gmail.com>
2017-06-30 13:27:03 -0400

    Initial commit

---
+++ easycsv.go
@@ -0,0 +1,28 @@
+// Package easycsv provides a quick wrapper for opening a csv file and returning
+// the data in an easy to use, iterable way.
+package easycsv
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+// Open takes the given path to a csv file and returns the rows as a [][]string which you can then
+// iterate over.
+func Open(path string) ([][]string, error) {
+	fn, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer fn.Close()
+
+	reader := csv.NewReader(fn)
+	fields, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return fields, nil
+}
